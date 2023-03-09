82c14ae9cf96979fda09ae833b02e0591d8704a6
GrishithaV <123852582+GrishithaV@users.noreply.github.com>
2023-03-09 10:53:47 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector('.fa-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.addEventListener('scroll', () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+});
+
+
+$(function() {
+    $('#datepicker').datepicker();
+});
+$(function() {
+    $('#datepicker1').datepicker();
+});
+$(function() {
+    $('#datepicker2').datepicker();
+});
